1a6717f9bba659b145656c6447c655113997ba3e
Matias <matias.beteta@applying.cloud>
2026-09-17 23:13:20 -0500

    fix: mover credenciales al archivo .env

--- config/users.js
+++ config/users.js
@@ -1,12 +1,14 @@
 'use strict'
 
+require('dotenv').config()
+
 module.exports = {
     standard_user: {
-        username: 'standard_user',
-        password: 'secret_sauce'
+        username: process.env.STANDARD_USER,
+        password: process.env.PASSWORD
     },
     locked_out_user: {
-        username: 'locked_out_user',
-        password: 'secret_sauce'
+        username: process.env.LOCKED_USER,
+        password: process.env.PASSWORD
     }
 }
